a5ccc07a48e64c3023fb72d7a1fbc37803a1eb7c
Gloria <zhan2209@users.noreply.github.com>
2021-01-27 17:07:18 -0600

    1244

---
+++ Java/src/HashMap/DesignALeaderboard_1244.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Leaderboard {
+    private static Map<Integer, Integer> scoreBoard ;
+    
+    public Leaderboard() {
+        scoreBoard = new HashMap<>();
+    }
+    
+    public void addScore(int playerId, int score) { 
+        scoreBoard.put(playerId ,
+                       scoreBoard.getOrDefault(playerId, 0) + score);
+    }
+    
+    public int top(int K) {
+        List<Integer> l = new ArrayList<Integer>(scoreBoard.values());
+        Collections.sort(l, Collections.reverseOrder());
+        l = l.subList(0,K);
+        return l.stream().collect(
+            Collectors.summingInt(Integer::intValue));
+    }
+    
+    public void reset(int playerId) {
+        scoreBoard.put(playerId,0);
+    }
+}
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
