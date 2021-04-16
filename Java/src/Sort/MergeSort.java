a79062e9e56437e09ceb1f5e0ba2f805dee718be
Glossaria <leetcode1024@gmail.com>
2021-04-15 20:52:46 -0500

    Merge sort with test

--- Java/src/Sort/MergeSort.java
+++ Java/src/Sort/MergeSort.java
@@ -4,4 +4,26 @@ package Sort;
  * Created on 1/26/21.
  */
 public class MergeSort {
+    public static int[] mergeSort(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] res = new int[len1 + len2];
+        int cur = 0, i = 0, j = 0;
+
+        while( i < len1 || j < len2 ) {
+            if (i >= len1) {
+                res[cur++] = arr2[j++];
+            } else if (j >= len2) {
+                res[cur++] = arr1[i++];
+            }else{
+                if(arr1[i] < arr2[j]){
+                    res[cur++] =  arr1[i++];
+                }else{
+                    res[cur++] = arr2[j++];
+                }
+            }
+        }
+        return res;
+    }
+
 }
---
+++ Java/test/Sort/MergeSortTest.java
@@ -0,0 +1,45 @@
+package Sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static Sort.MergeSort.mergeSort;
+import static Utils.SortUtils.generateRandomArray;
+import static org.junit.Assert.assertArrayEquals;
+
+public class MergeSortTest {
+
+    @Test
+    public void testMergeSort(){
+        int[] case1 = generateRandomArray(11);
+        int[] case2 = generateRandomArray(11);
+        Arrays.sort(case1);
+        Arrays.sort(case2);
+        int[] mergedArray = new int[case1.length + case2.length];
+        System.arraycopy( case1, 0, mergedArray, 0, case1.length);
+        System.arraycopy( case2, 0, mergedArray, case1.length, case2.length );
+        Arrays.sort(mergedArray);
+        int[] res = mergeSort(case1, case2);
+        assertArrayEquals(res, mergedArray);
+    }
+
+    @Test
+    public void testMultipleTimes(){
+        for(int i = 0; i < 10; i++){
+            int[] case1 = generateRandomArray(80);
+            int[] case2 = generateRandomArray(100);
+            Arrays.sort(case1);
+            Arrays.sort(case2);
+
+            int[] mergedArray = new int[case1.length + case2.length];
+            System.arraycopy( case1, 0, mergedArray, 0, case1.length);
+            System.arraycopy( case2, 0, mergedArray, case1.length, case2.length );
+            Arrays.sort(mergedArray);
+
+            int[] res = mergeSort(case1, case2);
+
+            assertArrayEquals(res, mergedArray);
+        }
+    }
+}
